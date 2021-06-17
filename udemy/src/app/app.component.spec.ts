2b7d78cd5c028d66e20e1e4411a37910df47c24e
mernstackdeveloper <asadahmed78692@gmail.com>
2021-06-17 12:18:15 +0530

    final commit

--- udemy/src/app/app.component.spec.ts
+++ udemy/src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
@@ -12,7 +12,7 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
-  }));
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'typing-game'`, () => {
+  it(`should have as title 'udemy'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('typing-game');
+    expect(app.title).toEqual('udemy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('typing-game app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('udemy app is running!');
   });
 });
