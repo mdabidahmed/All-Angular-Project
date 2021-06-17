ef5466d031abd48e3accb482b5e9e07573a12c91
mernstackdeveloper <49354823+mernstackdeveloper@users.noreply.github.com>
2021-06-17 12:25:28 +0530

    Delete comps directory

--- comps/src/app/elements/times.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TimesDirective } from './times.directive';
-
-describe('TimesDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TimesDirective();
-    expect(directive).toBeTruthy();
-  });
-});
